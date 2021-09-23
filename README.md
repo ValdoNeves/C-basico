a61cfd7c60aa1bbf12a3d8204b5c044fb917da9d
ValdoNeves <valdogt1@gmail.com>
2021-09-23 14:46:04 -0300

    atualização do readme, e finalização da logica de laços de repetição

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-__Lógica básica desenvolvida em C/C++__
+# __Lógica básica desenvolvida em C/C++__ 
+
+
+>Nota de alunos
+* **Condições**.
+
+>Calculadora
+* **Operações ***básicas*** entre dois números**, mas sem tratamento.
+
+>Índice de Poluição
+* A partir de uma medição, um tipo de alerta será enviada a um determinado grupo de empresas.
+
+>Faça etária
+* **Condições**.
+
+>Bônus Salarial
+* Dependendo do tempo de trabalho uma porcentagem deve ser adicionada ao seu salário atual em forma de bônus.
+
+>Ano Bissexto
+* Digitar um ano e verificar se o mesmo é bissexto ou não.
+
+>Multa de Transito
+* Um valor x em reais deve ser exibido dependendo da proporção ultrapassada do limite da via.
+
+>Caixa eletrônico
+* Saldo;
+* Deposito;
+* Saque, ***com validação***.
+
+>Laço de repetição
+1.  Ler valores inteiros do teclado;
+ 
+* Quando o zero for escolhido o laço deve ser finalizado;
+2. exibir;
+* Menor valor;
+* Maior valor;
+* Media dos valores.
+
+>Números ímpares
+* Exibir os números ímpares entre 100 e 200.
+
+>Tabuada
+* Digitar um valor inteiro;
+* Exibir a tabuada do número escolhido iniciando em 1 e finalizando em 10.
+
--- c/laco.c
+++ c/laco.c
@@ -5,13 +5,16 @@ int main(){
   int numero;
   int cont = 0;
   int resultado = 0;
-  int somatoria = 0;
+  float somatoria = 0.0;
 
   printf("Digite um numero inteiro: \n");
   scanf("%d", &numero);
+
   int menor = numero;
   int maior = 0;
   float media = 0;
+  float mediaTotal = 0;
+
   while (numero > 0){
     if(menor > numero ){
       menor = numero;
@@ -19,26 +22,25 @@ int main(){
     if(maior < numero){
       maior = numero;
     }
-    if(media == 0){
-      media = numero;
-      
-    }else{
-      resultado = somatoria / cont;
-
-    }
     cont++; 
     somatoria = somatoria + numero;
     printf("Insira mais um numero inteiro: \n");
     scanf("%d", &numero);
-    
-
   }
 
-  printf("\n%d contador\n", cont);
-  printf("\n%d somatoria media\n", somatoria);
-  printf("\n%d é o menor valor\n", menor);
-  printf("\n%d é o maior valor\n", maior);
-  printf("\n%.2f é o media valor\n", media);
+  mediaTotal = somatoria / cont;
+  media = (maior + menor) / 2.0; 
 
+  printf("\n--------------------------------------------\n");
+
+  printf("\nForam digitados no total %d\n numeros inteiros antes de finalizar o laco\n", cont);
+  // printf("\n%.2f somatoria media\n", somatoria);
+  printf("\nMenor valor digitado %d, \n", menor);
+  printf("\nMaior valor digitado %d,\n", maior);
+  printf("\nMedia entre maior e menor: %.2f\n", media);
+  printf("\nMedia total: %.2f\n", mediaTotal);
+  
   printf("\nFim\n");
+  printf("\n--------------------------------------------\n");
+
 }
